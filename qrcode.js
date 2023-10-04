cc57462a8e4e8503e612d3909babd81dd46e625c
PankajBhatta <96721563+PankajBhatta@users.noreply.github.com>
2023-10-04 11:18:50 +0545

    Add files via upload

---
+++ qrcode.js
@@ -0,0 +1,32 @@
+
+const QRCode = require('qrcode')
+
+
+let data = {
+	name:"pankaj bhatta",
+	age:22,
+	department:"BCA",
+	id:"192019"
+}
+// Converting the data into String format
+let stringdata = JSON.stringify(data)
+
+// Print the QR code to terminal
+QRCode.toString(stringdata,{type:'terminal'},
+					function (err, QRcode) {
+
+	if(err) return console.log("error occurred")
+
+	// Printing the generated code
+	console.log(QRcode)
+})
+
+
+
+// Converting the data into base64
+QRCode.toDataURL(stringdata, function (err, code) {
+	if(err) return console.log("error occurred")
+
+	// Printing the code
+	console.log(code)
+})
