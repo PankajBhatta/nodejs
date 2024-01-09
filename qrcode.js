5b6dcf45f26f8c7eb3d7364c1042e8c9f2d19f48
PankajBhatta <96721563+PankajBhatta@users.noreply.github.com>
2024-01-09 13:41:45 +0545

    just trry

--- qrcode.js
+++ qrcode.js
@@ -1,32 +1,35 @@
 
-const QRCode = require('qrcode')
+// const QRCode = require('qrcode')
 
 
-let data = {
-	name:"pankaj bhatta",
-	age:22,
-	department:"BCA",
-	id:"192019"
-}
-// Converting the data into String format
-let stringdata = JSON.stringify(data)
+// let data = {
+// 	name:"pankaj bhatta",
+// 	age:22,
+// 	department:"BCA",
+// 	id:"192019"
+// }
+// // Converting the data into String format
+// let stringdata = JSON.stringify(data)
 
-// Print the QR code to terminal
-QRCode.toString(stringdata,{type:'terminal'},
-					function (err, QRcode) {
+// // Print the QR code to terminal
+// QRCode.toString(stringdata,{type:'terminal'},
+// 					function (err, QRcode) {
 
-	if(err) return console.log("error occurred")
+// 	if(err) return console.log("error occurred")
 
-	// Printing the generated code
-	console.log(QRcode)
-})
+// 	// Printing the generated code
+// 	console.log(QRcode)
+// })
 
 
 
-// Converting the data into base64
-QRCode.toDataURL(stringdata, function (err, code) {
-	if(err) return console.log("error occurred")
+// // Converting the data into base64
+// QRCode.toDataURL(stringdata, function (err, code) {
+// 	if(err) return console.log("error occurred")
 
-	// Printing the code
-	console.log(code)
-})
+// 	// Printing the code
+// 	console.log(code)
+// })
+
+
+console.log("Hello world just try")
